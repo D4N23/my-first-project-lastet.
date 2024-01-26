b286febb937c4ced0f4fb7ecd001de0813809c10
Danilo <daniilosam23@gmail.com>
2024-01-26 17:10:15 -0300

    isso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/main/home/home.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
+  template:`<app-navbar/>
+            <app-home></app-home>
+        <app-footer></app-footer>
+            `
+
+  ,
 })
 export class AppComponent {
   title = 'my-first-project-lastet.';
